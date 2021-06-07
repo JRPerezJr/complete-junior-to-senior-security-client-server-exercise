211be4e96b493cf13cf3005a4fa8fa33c971fdd0
Juan <pront23@gmail.com>
2021-06-07 19:55:52 +0900

    added ip logging

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const helmet = require('helmet');
 const winston = require('winston');
 
+const PORT = 3000;
+
 const bodyParser = require('body-parser');
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'index.html'))
+);
 
 app.post('/secret', (req, res) => {
+  let forwarded = req.headers['x-forwarded-for'];
+  let ip = forwarded ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
   const { userInput } = req.body;
   console.log(userInput);
   if (userInput) {
-    winston.log('info', 'user input: ' + userInput);
+    winston.log('info', `user ip: ${ip} with user input: ${userInput}`);
     res.status(200).json('success');
   } else {
-    winston.error('This guy is messing with us:' + userInput);
+    winston.error('This guy at ' + ip + ' is messing with us:' + userInput);
     res.status(400).json('incorrect submission');
   }
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`🦄 Flying high on port ${PORT}`));
